4bcca5880e0d0392c1b72bc6b4773af9057b83d5
Ionut Enescu <ionut.462.enescu@gmail.com>
2022-09-09 16:19:32 +0300

    zipkin, rabbitmq

--- src/main/java/com/springms/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/springms/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.springms.microservices.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,9 +19,12 @@ public class CurrencyExchangeController {
     @Autowired
     private CurrencyExchangeRepository currencyExchangeRepository;
 
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
     @GetMapping("currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
 //        CurrencyExchange ce = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+        logger.info("from: {}, to: {}", from, to);
         CurrencyExchange ce = currencyExchangeRepository.findByFromAndTo(from, to);
         if (ce == null) {
             throw new RuntimeException("Unable to find data for " + from  + " and " + to);
